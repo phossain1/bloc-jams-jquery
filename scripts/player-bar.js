2d04b7ae766cfcece9f79cff9ce097a3f6f0720c
Pinky Hossain <monika.hossain27@gmail.com>
2020-02-14 13:34:38 -0500

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -41,5 +41,6 @@
     const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
+    $('#time-control .total-time').text (duration);
   }, 1000);
 }
